e7e0ffa31722caf823c67e3caffdc3dc59b1576a
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-02-16 16:36:37 -0500

    Create Solution.java

---
+++ 021/Solution.java
@@ -0,0 +1,42 @@
+// Iteration
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode cur1 = l1;
+        ListNode cur2 = l2;
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (cur1 != null || cur2 != null) {
+            if (cur1 != null && cur2 != null) {
+                if (cur1.val < cur2.val) {
+                    cur.next = new ListNode(cur1.val);
+                    cur = cur.next;
+                    cur1 = cur1.next;
+                }
+                else {
+                    cur.next = new ListNode(cur2.val);
+                    cur = cur.next;
+                    cur2 = cur2.next;
+                }
+            }
+            else if (cur1 == null) {
+                cur.next = new ListNode(cur2.val);
+                cur = cur.next;
+                cur2 = cur2.next;
+            }
+            else {
+                cur.next = new ListNode(cur1.val);
+                cur = cur.next;
+                cur1 = cur1.next;
+            }
+        }
+        return dummy.next;
+    }
+}
