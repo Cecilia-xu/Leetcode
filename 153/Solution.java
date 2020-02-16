a43bdd292c055bb11db9b7ecc6de1d747d8b9a12
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-02-16 14:57:57 -0500

    Create  Solution.java

---
+++ 153/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMin(int[] nums) {
+        return findLocalMin(nums, 0, nums.length - 1);
+    }
+    
+    public int findLocalMin(int[] nums, int left, int right) {
+        if (left + 1 >= right) {
+            return Math.min(nums[left], nums[right]);
+        }
+        
+        if (nums[left] < nums[right]) {
+            return nums[left];
+        }
+        
+        int mid = left + (right - left) / 2;
+        return Math.min(findLocalMin(nums, left, mid), 
+                        findLocalMin(nums, mid + 1, right));
+    }
+}
