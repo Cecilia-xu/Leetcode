e5a146e5fa71baf6128bcea232c94c11aec77220
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-02-17 00:42:17 -0500

    Create  Solution.java

---
+++ 160/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode cur1 = headA;
+        ListNode cur2 = headB;
+        while (cur1 != cur2) {
+            cur1 = (cur1 == null ? headB : cur1.next);
+            cur2 = (cur2 == null ? headA : cur2.next);
+        }
+        return cur1;
+    }
+}
