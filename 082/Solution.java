86716b71b11d23304676344e08efe45143da16c8
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-02-17 16:15:15 -0500

    Create Solution.java

---
+++ 082/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode tail = dummy;
+        ListNode slow = dummy.next;
+        ListNode fast = dummy.next;
+        while (fast != null) {
+            while (fast.next != null && fast.next.val == slow.val) {
+                fast = fast.next;
+            }
+            if (fast == slow) {
+                tail.next = slow;
+                tail = tail.next;
+            }
+
+            slow = fast.next;
+            fast = fast.next;
+        }
+        
+        if (tail != null) {
+            tail.next = null;
+        }
+        return dummy.next;
+    }
+}
