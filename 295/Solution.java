d1f264564b94ab672a570aa6f9ae6f48dedfa42f
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-02-20 00:01:36 -0500

    Create Solution.java

---
+++ 295/Solution.java
@@ -0,0 +1,66 @@
+class MedianFinder {
+    
+    PriorityQueue<Integer> maxHeap;
+    PriorityQueue<Integer> minHeap;
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        this.maxHeap = new PriorityQueue<>((n1, n2) -> (n2 - n1));
+        this.minHeap = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        if (maxHeap.isEmpty() || num <= maxHeap.peek()) {
+            maxHeap.add(num);
+        }
+        
+        if (num > maxHeap.peek()) {
+            minHeap.add(num);
+        }
+        
+    }
+    
+    public double findMedian() {
+        int size = maxHeap.size() + minHeap.size();
+       
+        if (size % 2 == 1) { 
+            if(maxHeap.size() > minHeap.size() + 1) {
+                while (maxHeap.size() > minHeap.size() + 1) {
+                    minHeap.add(maxHeap.poll());
+                }
+            }
+            else {
+                while (maxHeap.size() < minHeap.size() + 1) {
+                    maxHeap.add(minHeap.poll());
+                } 
+            }
+        }
+        else {
+            if(maxHeap.size() > minHeap.size()) {
+                while (maxHeap.size() > minHeap.size()) {
+                    minHeap.add(maxHeap.poll());
+                }
+            }
+            else {
+                while (maxHeap.size() < minHeap.size()) {
+                    maxHeap.add(minHeap.poll());
+                }
+            }
+        }
+        
+        
+        if (maxHeap.size() == minHeap.size() + 1) {
+            return (double)maxHeap.peek();
+        }
+        else {
+            return (double)(maxHeap.peek() + minHeap.peek()) / 2;
+        }
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
