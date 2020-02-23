a40b754fa8592e63d5492eb7ae297f4fbe1bd93c
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-02-23 17:58:26 -0500

    Update Solution.java

--- 153/Solution.java
+++ 153/Solution.java
@@ -1,3 +1,4 @@
+// Solution 1: template 2
 class Solution {
     public int findMin(int[] nums) {
         int left = 0, right = nums.length - 1;
@@ -17,5 +18,22 @@ class Solution {
         else {
             return nums[right];
         }
+    }
+}
+// Solution 2: template 1
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= nums[nums.length - 1]) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        
+        return left < 0 ? nums[0] : nums[left];
     }
 }
