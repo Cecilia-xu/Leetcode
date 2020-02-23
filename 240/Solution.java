c0f83cd31ecba8a44dcf6734af8a453e1786ca9a
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-02-23 13:21:09 -0500

    Create Solution.java

---
+++ 240/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int row = matrix.length - 1, col = 0;
+        while(row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            else if (matrix[row][col] > target) {
+                row --;
+            }
+            else {
+                col ++;
+            }
+        }
+        return false;
+    }
+}
