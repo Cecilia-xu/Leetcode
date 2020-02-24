cc51ad76315db65358052d51387152ffc9587121
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-02-24 00:21:43 -0500

    Update Solution.java

--- 153/Solution.java
+++ 153/Solution.java
@@ -37,3 +37,24 @@ class Solution {
         return left < 0 ? nums[0] : nums[left];
     }
 }
+// Solution 3: divide and conquer
+class Solution {
+    public int findMin(int[] nums) {
+        return findLocalMin(nums, 0, nums.length - 1);
+    }
+    
+    public int findLocalMin(int[] nums, int left, int right) {
+        // base case 1: when the searching size is reduced to 1/2, return the smaller one
+        if (left + 1 >= right) {
+            return Math.min(nums[left], nums[right]);
+        }
+        // base case 2: when the whole searching space is sorted, return the smallest one
+        if (nums[left] < nums[right]) {
+            return nums[left];
+        }
+        // recursion rule: find the smaller one in two smaller searching space
+        int mid = left + (right - left) / 2;
+        return Math.min(findLocalMin(nums, left, mid), 
+                        findLocalMin(nums, mid + 1, right));
+    }
+}
