eaed6137c93d59c454b84a00a8ce20bd323bde17
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-02-24 15:26:28 -0500

    Update Solution.java

--- 081/Solution.java
+++ 081/Solution.java
@@ -57,3 +57,54 @@ class Solution {
         }
     }
 }
+// Solution 3: find minimum && last unduplicate number + binary search
+class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left != right && nums[left] == nums[right]) {
+            right--;
+        }
+        if (right == 0) {
+            return nums[right] == target;
+        }
+        int minIdx = findMin(nums, left, right);
+        if (target > nums[right]) {
+            return minIdx - 1 >= left && binarySearch(nums, target, left, minIdx - 1);
+        }
+        else {
+            return right >= minIdx && binarySearch(nums, target, minIdx, right);
+        }
+    }
+    
+    public int findMin(int[] nums, int left, int right) {
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return Math.min(nums[left], nums[right]) == nums[left] ? left : right;
+    }
+    
+    public boolean binarySearch(int[] nums, int target, int left, int right) {
+         while (left + 1 < right) {
+             int mid = left + (right - left) / 2;
+             if (nums[mid] == target) {
+                 return true;
+             } 
+             else if (nums[mid] > target) {
+                 right = mid;
+             }
+             else {
+                 left = mid;
+             }
+         }
+        return  nums[left] == target || nums[right] == target;
+    }
+}
