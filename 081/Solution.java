92440eb996cdcb27f91750b51592501a7496a3c4
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-02-24 15:08:36 -0500

    Update Solution.java

--- 081/Solution.java
+++ 081/Solution.java
@@ -9,3 +9,51 @@ class Solution {
         return false;
     }
 }
+// Solution 2: binary search
+class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left != right && nums[left] == nums[right]) {
+            left++;
+        }
+        if (left == nums.length - 1) {
+            return nums[left] == target;
+        }
+        int start = nums[left], end = nums[nums.length - 1];
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            else if (nums[mid] >= start) {
+                if (target >= start && nums[mid] > target) {
+                    right = mid;
+                }
+                else {
+                    left = mid;
+                }
+            }
+            else {
+                if (target <= end && nums[mid] < target) {
+                    left = mid;
+                }
+                else {
+                    right = mid;
+                }
+            }
+        }
+        
+        if (nums[left] == target) {
+            return true;
+        }
+        else if (nums[right] == target) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
