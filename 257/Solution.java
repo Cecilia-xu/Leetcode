bdd9414b2b96813a61ebfb7acba6507233e1451e
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-02-25 15:46:11 -0500

    Create Solution.java

---
+++ 257/Solution.java
@@ -0,0 +1,30 @@
+// Solution 1: recursion
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<>();
+        if (root == null) {
+            return paths;
+        }
+        List<String> leftPaths = binaryTreePaths(root.left);
+        List<String> rightPaths = binaryTreePaths(root.right);
+        for (String path : leftPaths) {
+            paths.add(root.val + "->" + path);
+        }
+        for (String path : rightPaths) {
+            paths.add(root.val + "->" + path);
+        }
+        if (paths.size() == 0) {
+            paths.add("" + root.val);
+        }
+        return paths;
+    }
+}
