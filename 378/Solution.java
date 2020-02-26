5dc23ec2e2a489a89e1d1943e8b9b4822d94ab09
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-02-26 13:38:51 -0500

    Update Solution.java

--- 378/Solution.java
+++ 378/Solution.java
@@ -1,4 +1,4 @@
-// Solution 1: binary search
+// Solution 1: binary search (not optimized: count one by one)
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int left = matrix[0][0];
@@ -23,7 +23,31 @@ class Solution {
         return left;
     }
 }
-// Solution 2: max heap
+// Solution 2: binary search (optimized: count by find the last element < mid in each row)
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int left = matrix[0][0], right = matrix[matrix.length - 1][matrix[0].length - 1];
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int count = 0;
+            for (int i = 0; i < matrix.length; i++) {
+                int j = matrix[0].length - 1; 
+                while(j >= 0 && matrix[i][j] > mid) {
+                    j--;
+                }
+                count += j + 1;
+            }
+            if (count < k) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
+// Solution 3: max heap
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int row = matrix.length;
