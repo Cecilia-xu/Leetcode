3b44543730737f2a7ebfc9c78d017a8540ba1570
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-02-28 13:36:18 -0500

    Create Solution.java

---
+++ 530/Solution.java
@@ -0,0 +1,30 @@
+// Solution: inorder traversal
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int getMinimumDifference(TreeNode root) {
+        ArrayList<Integer> inorderList = new ArrayList<>();
+        inorderTraversal(root, inorderList);
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < inorderList.size(); i++) {
+            min = Math.min(inorderList.get(i) - inorderList.get(i - 1), min);
+        }
+        return min;
+    }
+    
+    public void inorderTraversal(TreeNode root, ArrayList<Integer> inorderList) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, inorderList);
+        inorderList.add(root.val);
+        inorderTraversal(root.right, inorderList);
+    }
+}
