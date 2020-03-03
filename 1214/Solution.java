770ec45d6c858d84534e9738036fe823786a41cd
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-03-03 16:52:18 -0500

    Update Solution.java

--- 1214/Solution.java
+++ 1214/Solution.java
@@ -34,3 +34,46 @@ class Solution {
         inorder(root.right, nums);
     }
 }
+// Solution 2: inorder + two pointers
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean twoSumBSTs(TreeNode root1, TreeNode root2, int target) {
+        ArrayList<Integer> nums1 = new ArrayList<>();
+        ArrayList<Integer> nums2 = new ArrayList<>();
+        inorder(root1, nums1);
+        inorder(root2, nums2);
+        if (nums1.size() == 0 || nums2.size() == 0) {
+            return false;
+        }
+        int start = 0, end = nums2.size() - 1;
+        while (start < nums1.size() && end >= 0) {
+            if (nums1.get(start) + nums2.get(end) == target) {
+                return true;
+            }
+            else if (nums1.get(start) + nums2.get(end) > target) {
+                end--;
+            }
+            else {
+                start++;
+            }
+        }
+        return false;
+    }
+    
+    private void inorder(TreeNode root, ArrayList<Integer> nums) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, nums);
+        nums.add(root.val);
+        inorder(root.right, nums);
+    }
+}
