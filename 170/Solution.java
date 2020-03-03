8426080c076a1a86b23f839fc0d425f6c6af1f49
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-03-03 14:11:20 -0500

    Update Solution.java

--- 170/Solution.java
+++ 170/Solution.java
@@ -1,5 +1,5 @@
 // Two sum: data structure design
-// Solution 1: similar to twoSum, use hashmap
+// Solution 1: similar to Two Sum I, use hashmap
 class TwoSum {
     private HashMap<Integer, Integer> map;
     
@@ -24,6 +24,46 @@ class TwoSum {
             }
             if (complement == entry.getKey() && entry.getValue() > 1) {
                 return true;
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */
+
+// Solution 2: similar to Two Sum II, use sorted array + two pointers
+class TwoSum {
+    private List<Integer> nums;
+    
+    /** Initialize your data structure here. */
+    public TwoSum() {
+        this.nums = new ArrayList<Integer>();
+    }
+    
+    /** Add the number to an internal data structure.. */
+    public void add(int number) {
+        nums.add(number);
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    public boolean find(int value) {
+        Collections.sort(nums);
+        int start = 0, end = nums.size() - 1;
+        while (start < end) {
+            if (nums.get(start) + nums.get(end) == value) {
+                return true;
+            }
+            else if (nums.get(start) + nums.get(end) < value) {
+                start++;
+            }
+            else {
+                end--;
             }
         }
         return false;
