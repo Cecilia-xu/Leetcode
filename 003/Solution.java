5cadbbbcfcac3633249018d39267e8bae358bb90
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-03-12 17:57:34 -0400

    Update Solution.java

--- 003/Solution.java
+++ 003/Solution.java
@@ -18,3 +18,22 @@ class Solution {
         return count == Integer.MIN_VALUE ? 0: count;
     }
 }
+// Solution 2: sliding window + int array(represent the frequency of characters)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int start, end = 0;
+        int count = Integer.MIN_VALUE;
+        for (start = 0; start < s.length(); start++) {
+            while (end < s.length() && !set.contains(s.charAt(end))) {
+                set.add(s.charAt(end));
+                end++;
+            }
+            if (end <= s.length()) {
+                count = Math.max(count, end - start);
+                set.remove(s.charAt(start));
+            }
+        }
+        return count == Integer.MIN_VALUE ? 0: count;
+    }
+}
