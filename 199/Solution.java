1b04e218b74a73e7dddee03c7e691d4a79d37da0
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-03-20 11:40:04 -0400

    Create Solution.java

---
+++ 199/Solution.java
@@ -0,0 +1,67 @@
+// BFS (the rightside node is the i-th node and i = the number of nodes in a specific level  - 1) 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>(); 
+        
+        if (root == null) {
+            return res;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                if (i == size - 1) {
+                    res.add(node.val);
+                }
+            }
+        }
+        return res;
+    }
+}
+// DFS: use depth trick
+// When adding a node to result, which means the depth == the size of result list before adding
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        viewHelper(res, root, 0);
+        return res;
+    }
+    
+    private void viewHelper(List<Integer> res, TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        if (depth == res.size()) {
+            res.add(root.val);
+        }
+        viewHelper(res, root.right, depth + 1);
+        viewHelper(res, root.left, depth + 1);
+    }
+}
