89b7c80dadd8f9c0a011ec4c1dacbe3a3ba45749
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-03-27 19:38:11 -0400

    Create Solution.java

---
+++ 036/Solution.java
@@ -0,0 +1,58 @@
+// Brute-Force solution
+// Time complexity: O(1)
+// Space complexity: O(1)
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[] visited = new boolean[9];
+        
+        // check by rows
+        for (int col = 0; col < 9; col++) {
+            Arrays.fill(visited, false);
+            for (int row = 0; row < 9; row++) {
+                if (!checkValid(visited, board[row][col])) {
+                    return false;
+                }
+            }
+        }
+        
+        // check by columns
+        for (int row = 0; row < 9; row++) {
+            Arrays.fill(visited, false);
+            for (int col = 0; col < 9; col++) {
+                if (!checkValid(visited, board[row][col])) {
+                    return false;
+                }
+            }
+        }
+        
+        // check by sub boxes
+        for (int row = 0; row < 9; row += 3) {
+            for (int col = 0; col < 9; col += 3) {
+                Arrays.fill(visited, false);
+                for (int k = 0; k < 9; k++) {
+                    if (!checkValid(visited, board[row + k / 3][col + k % 3])) {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    private boolean checkValid(boolean[] visited, char cell) {
+        if (cell == '.') {
+            return true;
+        }
+        else if (cell < '1' && cell >'9') {
+            return false;
+        }
+        else if (visited[cell - '1']) {
+            return false;
+        }
+        else {
+            visited[cell - '1'] = true;
+            return true;
+        }
+    }
+}
