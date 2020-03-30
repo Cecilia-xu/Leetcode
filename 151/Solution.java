68d7f22890f06bb8568c18f8877b6324da9315fb
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-03-29 22:54:32 -0400

    Create Solution.java

---
+++ 151/Solution.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String reverseWords(String s) {
+        s = s.trim();
+        if (s == null || s.length() <= 1) {
+            return s;
+        }
+        char[] characters = s.toCharArray();
+        int start = 0;
+        while (start < characters.length) {
+            int end = start;
+            while (end < characters.length && characters[end] != ' ') {
+                end++;
+            }
+            reverse(characters, start, end - 1);
+            while (end < characters.length && characters[end] == ' ') {
+                end++;
+            }
+            start = end;
+        }
+        reverse(characters, 0, characters.length - 1);
+       int right = removeSpaces(characters);
+        return new String(characters).substring(0, right);
+    }
+    
+    private void reverse(char[] arr, int start, int end) {
+        while (start < end) {
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    
+    private int removeSpaces(char[] arr) {
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (!(arr[i] == ' ' && arr[i - 1] == ' ')) {
+                arr[j] = arr[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
