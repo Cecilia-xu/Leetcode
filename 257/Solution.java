c7867f2a68d6649f5eda6cef9064b30245d40426
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-04-03 02:09:05 -0400

    Update Solution.java

--- 257/Solution.java
+++ 257/Solution.java
@@ -1,7 +1,7 @@
-// Solution 1: recursion + divide and conquer
+// Solution 1: recursion + divide and conquer + String
 // Time complexity : O(n)
 // Space complexity: O(H). Best case: O(logn). Worst Case: O(n)
-// Note: I do not think it is a good solution because the use of String!
+// Note: Not good enough. Because using immutable string is very costly.
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -29,5 +29,40 @@ class Solution {
             paths.add("" + root.val);
         }
         return paths;
+    }
+}
+// Solution 2: Recursion + Traversal(dfs) + stringbuilder optimization
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<String>();
+        StringBuilder sb = new StringBuilder();
+        searchPaths(root, sb, result);
+        return result;
+    }
+    
+    private void searchPaths(TreeNode root, StringBuilder sb, List<String> res) {
+        if (root == null) {
+            return;
+        }
+        int len = sb.length();
+        sb.append("" + root.val);
+        if (root.left == null && root.right == null) {
+            res.add(sb.toString());
+        }
+        else {
+            sb.append("->");
+            searchPaths(root.left, sb, res);
+            searchPaths(root.right, sb, res);
+        }
+        sb.setLength(len);
     }
 }
