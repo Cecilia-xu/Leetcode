4765127bbc42c41ef98dbadcd272b3b36409b191
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-04-04 20:30:29 -0400

    Create Convert Binary Tree to Linked Lists by Depth.java

---
+++ Convert Binary Tree to Linked Lists by Depth.java
@@ -0,0 +1,51 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root the root of binary tree
+     * @return a lists of linked list
+     */
+    public List<ListNode> binaryTreeToLists(TreeNode root) {
+        // Write your code here
+        List<ListNode> nodesList = new ArrayList<>();
+        if (root == null) {
+            return nodesList;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ListNode dummy = new ListNode(0);
+            ListNode cur = dummy;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                cur.next = new ListNode(node.val);
+                cur = cur.next;
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            cur.next = null;
+            nodesList.add(dummy.next);
+        }
+        return nodesList;
+    }
+}
