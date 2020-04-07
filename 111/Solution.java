48e27eeae85be2057072e26d5aed6004a2cfa911
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-04-07 13:47:04 -0400

    Update Solution.java

--- 111/Solution.java
+++ 111/Solution.java
@@ -1,4 +1,4 @@
-// Solution 1: recursion + divide and conquer
+// Solution 1: recursion + divide and conquer (not effecient compared with bfs)
 // Time complexity: O(n). Visit each nodes once
 // Space complexity: O(H). Worst case: O(n). when the tree is skewed, the height is n.
 /**
@@ -27,5 +27,46 @@ class Solution {
         else {
             return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
         }
+    }
+}
+// Solution 2: Iteration + BFS (much better)
+// If the tree is unbalanced then we will find the min height when we first reach a leaf, without exploring longer paths.
+// Time complexity: O(n). When tree is skewed, we need to offer and poll every nodes.
+// Space complexity: O(n). When tree is a perfect tree, the size of queue should be the number of nodes in the last level.
+// The height is logn, the number of nodes in the last level is 2^logn = n.
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int level = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    return level;
+                }
+                if (node.left != null) { 
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);                    
+                }
+            }
+            level++;
+        }
+        return level;
     }
 }
