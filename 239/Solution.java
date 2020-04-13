e6e2f45ccf2229f81812edd681536e9b85cd4379
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-04-13 00:54:01 -0400

    Create Solution.java

---
+++ 239/Solution.java
@@ -0,0 +1,21 @@
+// Solution 1: Max Heap (not the optimal solution)
+// Time complexity: O(nlogk). Poll and remove in O(logk) * (n - k) + heapify O(k) -> O(nlogk)
+// Space complexity: O(n). heap O(k) + result array O(n - k + 1) -> O(n)
+// Note: Heap can contain duplicate elements! Also, if we call remove(element), just remove one element!
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((n1, n2) -> n2 - n1);    
+        for (int i = 0; i < k - 1; i++) {
+            maxHeap.add(nums[i]);
+        }       
+        int[] res = new int[nums.length - k + 1];
+        int index = 0;
+        for (int i = k - 1; i < nums.length; i++) {
+            maxHeap.add(nums[i]);
+            res[index++] = maxHeap.peek();
+            int start = nums[i - k + 1];
+            maxHeap.remove(start);
+        }
+        return res;
+    }
+}
