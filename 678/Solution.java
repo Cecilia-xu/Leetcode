063d7142278b17dffe510f5bae6d6c93d04ce8db
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-04-16 16:35:03 -0400

    Update Solution.java

--- 678/Solution.java
+++ 678/Solution.java
@@ -31,3 +31,7 @@ class Solution {
         return minOpenCount == 0;
     }
 }
+// Other explanation: Have two counters one counter (cmax) for counting the maximum number of right braces we can accommodate with current left braces and stars.
+//Have a second counter(cmin) which represents the minimum number of right braces that must be there further to make sure the whole string is valid(this number can’t be negative, so if it becomes negative then we put it to zero.
+//At any time, if cmax becomes negative, it means we can’t accommodate current right braces with current left braces and stars. So, we return false.
+//In the end if cmin is positive then it means that at least there should be a cmin number of right braces to make sure the overall string is valid. So, we check whether cmin is zero or not and return the answer
