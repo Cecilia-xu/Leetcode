1bafabd9aef9206dadd3a9de34f5c0d2f74931f2
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-04-21 23:49:57 -0400

    Create Solution.java

---
+++ 198/Solution.java
@@ -0,0 +1,21 @@
+// Solution: dynamic programming
+// Time complexity: O(n)
+// Space complexity: O(n)
+class Solution {
+    public int rob(int[] nums) {
+        int[] curMoney = new int[nums.length + 1];
+        curMoney[0] = 0;
+        for (int i = 1; i <= nums.length; i++) {
+            if (i == 1) {
+                curMoney[i] = nums[i - 1];
+            }
+            else if (i == 2) {
+                curMoney[i] = Math.max(curMoney[1], nums[i - 1]);
+            }
+            else {
+                curMoney[i] = Math.max(curMoney[i - 1], curMoney[i - 2] + nums[i - 1]);
+            }
+        }
+        return curMoney[nums.length];
+    }
+}
