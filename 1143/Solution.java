7370b5643898bb10659dad33fc98ba807d006863
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-04-26 11:30:00 -0400

    Update Solution.java

--- 1143/Solution.java
+++ 1143/Solution.java
@@ -8,20 +8,19 @@ class Solution {
             return longestCommonSubsequence(text2, text1);
         }
         int[][] LCS = new int[len1 + 1][len2 + 1];
-        // Initialization
-        for (int i = 0; i <= len1; i++) {
-            LCS[i][0] = 0;
-        }
-        for (int j = 0; j <= len2; j++) {
-            LCS[0][j] = 0;
-        }
+        // // Initialization
+        // for (int i = 0; i <= len1; i++) {
+        //     LCS[i][0] = 0;
+        // }
+        // for (int j = 0; j <= len2; j++) {
+        //     LCS[0][j] = 0;
+        // }
         for (int i = 1; i <= len1; i++) {
             for (int j = 1; j <= len2; j++) { 
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
                     LCS[i][j] = LCS[i - 1][j - 1] + 1;
-                    continue;
                 }
-                LCS[i][j] = Math.max(LCS[i - 1][j], LCS[i][j - 1]);
+                LCS[i][j] = Math.max(LCS[i][j], Math.max(LCS[i - 1][j], LCS[i][j - 1]));
             }
         }
         return LCS[len1][len2];
