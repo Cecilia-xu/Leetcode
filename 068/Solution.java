5f593783d05f98c5ece3ab338c774c2484e0cff0
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-04-26 15:15:32 -0400

    Create Solution.java

---
+++ 068/Solution.java
@@ -0,0 +1,83 @@
+// Solution: String
+// Time complexity: O(n)
+// Space complexity: O(n)
+// Note: Need to notice that the line which just contains one word is similar to the last line case! If use the method in 
+// case 2, computing evenSpaces will be wrong casue the divisor is 0
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> result = new ArrayList<>();
+        int curLen = 0, wordCount = 0, curStart = 0;
+        boolean isLastLine = false;
+        for (int i = 0; i < words.length; i++) {
+            // check the words for a line until we find all words
+            while (i < words.length && curLen + words[i].length() <= maxWidth) {
+                if (curLen == 0) {
+                    curStart = i;
+                }
+                curLen += words[i].length();
+                wordCount++;
+                i++;
+            }
+            // check whether the space for spaces is guaranteed. If not, remove words until the spaces can be guaranteed.
+            while (curLen + wordCount - 1 > maxWidth) {
+                curLen -= words[i - 1].length();
+                wordCount--;
+                i--;
+            }
+            // check whether the line we checked is the last line 
+            if (i == words.length) {
+                isLastLine = true;
+            }
+            // compute the number of spaces
+            int spaceCount = maxWidth - curLen;
+            
+            // construct the string for each line and add it to the result
+            String line = constructLine(words, curStart, i, wordCount, spaceCount, isLastLine);
+            result.add(line);
+            
+            // recover variables for checking the next line
+            i--;
+            curLen = 0;
+            wordCount = 0;
+        }
+        
+        return result;
+    }
+    
+    private String constructLine(String[] words, int start, int end, int wordCount, int spaceCount, boolean isLastLine) {
+        StringBuilder sb = new StringBuilder();
+        // Case 1: last line or just one word for a line*
+        if (isLastLine || wordCount == 1) {
+            for (int i = start; i < end; i++) {
+                sb.append(words[i]);
+                if (spaceCount > 0) {
+                    sb.append(" ");
+                    spaceCount--;
+                }
+            }
+            while (spaceCount > 0) {
+                sb.append(" ");
+                spaceCount--;
+            }
+            return sb.toString();
+        }
+        // Case 2: not last line && more than one word
+        // assign spaces
+        int evenSpaces = spaceCount / (wordCount - 1);
+        int moreSpaces = spaceCount % (wordCount - 1);
+        for (int i = start; i < end; i++) {
+            sb.append(words[i]);
+            if (i != end - 1) {
+                for (int j = 0; j < evenSpaces; j++) {
+                    sb.append(" ");
+                }
+            }
+            if (moreSpaces > 0) {
+                sb.append(" ");
+                moreSpaces--;
+            }
+        }
+        return sb.toString();
+    }
+    
+}
