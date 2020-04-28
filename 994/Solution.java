be546e51ab5f6bf67ff505ee675a9a7d58989e23
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-04-27 20:45:20 -0400

    Update Solution.java

--- 994/Solution.java
+++ 994/Solution.java
@@ -1,50 +1,63 @@
-// Solution: BFS (明天重写！)
+// Solution: BFS 
+// Time complexity: O(m * n) m = row, n = col
+// Space complexity: O(m * n)
+// Note: The grid is just a state. Our initial state is the initial grid. 
+// The termination state is the state when there is nofresh oranges.
+// We do not need HashSet in this question because the value in each cell can represent the state. Not visited - 1, Visited- 2.
 class Solution {
     public int orangesRotting(int[][] grid) {
-        if(grid == null || grid.length == 0) return 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+       
+        // Initial state
         Queue<int[]> queue = new LinkedList<>();
-        int count_fresh = 0;
-        //Put the position of all rotten oranges in queue
-        //count the number of fresh oranges
-        for(int i = 0 ; i < rows ; i++) {
-            for(int j = 0 ; j < cols ; j++) {
-                if(grid[i][j] == 2) {
-                    queue.offer(new int[]{i , j});
+        int countFresh = 0;
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    countFresh++;
                 }
-                else if(grid[i][j] == 1) {
-                    count_fresh++;
+                else if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
                 }
             }
         }
-        //if count of fresh oranges is zero --> return 0 
-        if(count_fresh == 0) return 0;
-        int count = 0;
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-        //bfs starting from initially rotten oranges
-        while(!queue.isEmpty()) {
-            ++count;
+        
+        // No fresh oranges
+        if (countFresh == 0) {
+            return 0;
+        }
+        
+        // rotting oranges
+        int minute = 0;
+        int[][] directions = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        while (!queue.isEmpty()) {
+            // get the size of rotten oranges at the previous minute 
             int size = queue.size();
-            for(int i = 0 ; i < size ; i++) {
-                int[] point = queue.poll();
-                for(int dir[] : dirs) {
-                    int x = point[0] + dir[0];
-                    int y = point[1] + dir[1];
-                    //if x or y is out of bound
-                    //or the orange at (x , y) is already rotten
-                    //or the cell at (x , y) is empty
-                        //we do nothing
-                    if(x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] == 0 || grid[x][y] == 2) continue;
-                    //mark the orange at (x , y) as rotten
-                    grid[x][y] = 2;
-                    //put the new rotten orange at (x , y) in queue
-                    queue.offer(new int[]{x , y});
-                    //decrease the count of fresh oranges by 1
-                    count_fresh--;
+            // current minute
+            minute++;
+            for (int i = 0; i < size; i++) {
+                int[] rotten = queue.poll();
+                for (int[] dir : directions) {
+                    int newx = rotten[0] + dir[0];
+                    int newy = rotten[1] + dir[1];
+                    if (inArea(newx, newy, grid) && grid[newx][newy] == 1) {
+                        grid[newx][newy] = 2;
+                        countFresh--;
+                        if (countFresh == 0) {
+                            return minute;
+                        }
+                        queue.offer(new int[]{newx, newy});
+                    }
                 }
             }
         }
-        return count_fresh == 0 ? count - 1 : -1;
+        return -1;
+    }
+    
+    private boolean inArea(int x, int y, int[][] grid) {
+        return x >= 0 && x < grid.length && y >= 0 && y < grid[0].length;
     }
 }
