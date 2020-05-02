9d9b749a969a411f747c02696344aa9a8acf2024
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-05-02 15:14:01 -0400

    Update Solution.java

--- 008/Solution.java
+++ 008/Solution.java
@@ -1,14 +1,14 @@
-// How to analyze corner cases in a good strucutre?
-// Step 1: character types - (1) space (2) sign (3) number (4) others
+// How to analyze corner cases in a good structure?
+// Step 1: character types - 1. space 2. sign 3. number 4. others
 // Step 2: expected valid input - (spaces/no spaces) + (no sign/one sign) + (numbers) + others (whatever we do not care)
 // Step 3: design the logic for checking
 // (1) remove leading and trailing spaces -> narrow down our checking for a structure: (no sign/one sign) + (numbers)
 // (2) if then we find a character that is neither sign nor digit -> return 0 (as invalid input)
 // (3) sign checking: we should and only should find no more than 1 sign -> find 2 signs: return 0 (as invalid input)
 // (4) number checking: construct numbers until no digits can be found
-//  (4.1) No digit can be found: return 0 (default value - as invalid input)
-//  (4.2) Find digits but the number may overflow: return Integer.MIN_VALUE/MAX_VALUE if the number starts overflow
-//  (4.3) Find valid digits: break the loop since we do not care characters after numbers
+//      (4.1) No digit can be found: return 0 (default value - as invalid input)
+//      (4.2) Find digits but the number may overflow: return Integer.MIN_VALUE/MAX_VALUE if the number starts overflow
+//      (4.3) Find valid digits: break the loop since we do not care characters after numbers
 class Solution {
     public int myAtoi(String str) {
         long number = 0;
