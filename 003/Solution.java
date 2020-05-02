812444a73864aaffbba6ae43b59bbc47a709f088
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-05-02 16:34:18 -0400

    Update Solution.java

--- 003/Solution.java
+++ 003/Solution.java
@@ -1,39 +1,43 @@
-// Solution 1: sliding window + hashset
-// Time complexity: o(n)
+// Solution 1: sliding window + hashset (Not the optimal solution)
+// Time complexity: O(n)
+// Space complexity: O(n)
+// Note: use template to avoid mistakes!
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         HashSet<Character> set = new HashSet<>();
-        int start, end = 0;
-        int count = Integer.MIN_VALUE;
+        int end = 0;
+        int maxLen = Integer.MIN_VALUE;
         for (start = 0; start < s.length(); start++) {
             while (end < s.length() && !set.contains(s.charAt(end))) {
                 set.add(s.charAt(end));
                 end++;
             }
-            if (end <= s.length()) {
-                count = Math.max(count, end - start);
-                set.remove(s.charAt(start));
-            }
+            maxLen = Math.max(maxLen, end - start);
+            set.remove(s.charAt(start));
         }
-        return count == Integer.MIN_VALUE ? 0: count;
+        return maxLen == Integer.MIN_VALUE ? 0: count;
     }
 }
 // Solution 2: sliding window + int array(represent the frequency of characters)
+// Time complexity: O(n)
+// Space complexity: O(256) -> O(1)
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashSet<Character> set = new HashSet<>();
-        int start, end = 0;
-        int count = Integer.MIN_VALUE;
-        for (start = 0; start < s.length(); start++) {
-            while (end < s.length() && !set.contains(s.charAt(end))) {
-                set.add(s.charAt(end));
+        char[] visited = new char[256];
+        char[] letters = s.toCharArray();
+        int end = 0;
+        int maxLen = Integer.MIN_VALUE;
+        
+        for (int start = 0; start < letters.length; start++) { 
+            while (end < letters.length && visited[letters[end]] == 0) {
+                visited[letters[end]]++;
                 end++;
             }
-            if (end <= s.length()) {
-                count = Math.max(count, end - start);
-                set.remove(s.charAt(start));
-            }
+            int len = end - start;
+            maxLen = Math.max(maxLen, len);
+            visited[letters[start]]--;
         }
-        return count == Integer.MIN_VALUE ? 0: count;
+        
+        return maxLen == Integer.MIN_VALUE ? 0 : maxLen;
     }
 }
