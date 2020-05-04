58807dc030d7247c86b5c5f087e43de4d74ba11a
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-05-04 16:38:03 -0400

    Update Solution.java

--- 004/Solution.java
+++ 004/Solution.java
@@ -1,12 +1,17 @@
 // Solution: binary search
+// Time complexity: O(n1logn1)
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length, n2 = nums2.length;
+        
+        // Why len1 < len2 ? Because m2 = half - m1, if m1 is very large, m2 may be out of bound.
         if (n1 > n2) {
             return findMedianSortedArrays(nums2, nums1);
         }
         int k = (n1 + n2 + 1) / 2;
         int left = 0, right = n1 - 1;
+        
+        // Use binary search to find two candidates of the first element in the second half 
         while (left <= right) {
             int m1 = left + (right - left) / 2;
             int m2 = k - m1;
@@ -21,6 +26,7 @@ class Solution {
         int m1 = left;
         int m2 = k - left;
         
+        // find the candidates (deal with index-out-of-bound error)
         int c1 = Math.max(m1 <= 0 ? Integer.MIN_VALUE : nums1[m1 - 1], 
                           m2 <= 0 ? Integer.MIN_VALUE : nums2[m2 - 1]);
         int c2 = Math.min(m1 >= n1 ? Integer.MAX_VALUE : nums1[m1], 
