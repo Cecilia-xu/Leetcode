01dd0dc394aea8ab495fbb7012c37cf1fb36a0b4
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-05-05 00:37:36 -0400

    Create Solution.java

---
+++ 012/Solution.java
@@ -0,0 +1,11 @@
+// Solution: simulation
+// How? find the corresponding string when we know the number on a specific digit 
+class Solution {
+    public String intToRoman(int n) {
+        String M[] = {"", "M", "MM", "MMM"};
+        String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return M[n / 1000] + C[(n / 100) % 10] + X[(n / 10) % 10] + I[n % 10];
+    }
+}
