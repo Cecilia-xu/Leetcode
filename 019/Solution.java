d025adfac851fe5082cc1f2b206ce652b975c3e1
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-05-05 00:51:41 -0400

    Update Solution.java

--- 019/Solution.java
+++ 019/Solution.java
@@ -12,25 +12,41 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        // if head is null
+        if (head == null) {
+            return head;
+        }
+        
+        // create a dummy head
         ListNode dummy = new ListNode(0);
         dummy.next = head;
+        
+        // initialize slow and quick pointers
         ListNode slow = dummy;
         ListNode quick = dummy;
-        int count = 0;
-        while (count <= n) {
+        while (n > 0) {
             quick = quick.next;
-            count++;
+            n--;
         }
-        while (quick != null) {
-            quick = quick.next;
+        
+        // traverse and find the previous node and next node of the target node
+        while (quick.next != null) {
             slow = slow.next;
+            quick = quick.next;
         }
+        
+        // remove(*slow the previoud node of our target node, so link it to slow.next.next NOT quick!! 
+        // Because n can be 1 and quick can stop at our target node.)
         slow.next = slow.next.next;
+        
         return dummy.next;
+        
     }
 }
