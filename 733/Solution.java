5adec94cafa0e26923286c0d1c02a44d050fa0c3
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-05-11 14:21:14 -0400

    Update Solution.java

--- 733/Solution.java
+++ 733/Solution.java
@@ -34,3 +34,42 @@ class Solution {
         return newx >= 0 && newx < row && newy >= 0 && newy < col;
     }
 }
+
+// Solution 2: DFS
+// Time complexity: O(m * n). Worst case: all pixels are the same color and need to be changed.
+// Space complexity: O(m * n). The number of call stacks.
+class Solution {
+    public int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int oldColor = image[sr][sc];
+        //***** This if condition is very important to avoid stack overflow problem!
+        if (oldColor == newColor) {
+            return image;         
+        }
+        dfs(image, sr, sc, oldColor, newColor);   
+        return image;
+
+    }
+    
+    private void dfs(int[][] image, int sr, int sc, int oldColor, int newColor) {
+        // base case
+        if (!inArea(sr, sc, image.length, image[0].length) || image[sr][sc] != oldColor) {
+            return;
+        }
+        // assign the value
+        if (image[sr][sc] == oldColor) {
+            image[sr][sc] = newColor;
+        }
+        
+        // recursion rule
+        for (int i = 0; i < 4; i++) {
+            int newSr = sr + directions[i][0];
+            int newSc = sc + directions[i][1];
+            dfs(image, newSr, newSc, oldColor, newColor);
+        }
+    }
+    
+    public boolean inArea(int sr, int sc, int row, int col) {
+        return sr >= 0 && sr < row && sc >= 0 && sc < col;
+    }
+}
