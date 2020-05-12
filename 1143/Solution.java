23e34c80fa0b043f592d7dcdf32eb56127da92c2
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-05-12 11:44:03 -0400

    Update Solution.java

--- 1143/Solution.java
+++ 1143/Solution.java
@@ -4,23 +4,22 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int len1 = text1.length(), len2 = text2.length();
-        if (len1 < len2) {
-            return longestCommonSubsequence(text2, text1);
-        }
         int[][] LCS = new int[len1 + 1][len2 + 1];
-        // // Initialization
-        // for (int i = 0; i <= len1; i++) {
-        //     LCS[i][0] = 0;
-        // }
-        // for (int j = 0; j <= len2; j++) {
-        //     LCS[0][j] = 0;
-        // }
-        for (int i = 1; i <= len1; i++) {
-            for (int j = 1; j <= len2; j++) { 
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    LCS[i][j] = LCS[i - 1][j - 1] + 1;
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                // empty string -> LCS = 0
+                if (i == 0 || j == 0) {
+                    LCS[i][j] = 0;
+                    continue;
                 }
-                LCS[i][j] = Math.max(LCS[i][j], Math.max(LCS[i - 1][j], LCS[i][j - 1]));
+                // case 1: the last letter in text1 is not in the LCS
+                // case 2: the last letter in text2 is not in the LCS
+                LCS[i][j] = Math.max(LCS[i - 1][j], LCS[i][j - 1]);
+                // Note: 前i个,所以对应的位置要-1
+                // Case 3: the last letters in text1 and text2 are the same.
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    LCS[i][j] = Math.max(LCS[i][j], LCS[i - 1][j - 1] + 1);
+                }
             }
         }
         return LCS[len1][len2];
