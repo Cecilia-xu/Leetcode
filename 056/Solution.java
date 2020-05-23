f5622df93aa2ffe01cb42e0084391a70c6b02233
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-05-23 15:19:45 -0400

    Update Solution.java

--- 056/Solution.java
+++ 056/Solution.java
@@ -1,3 +1,39 @@
+// Solution: Intervals (sort start pos + compare and get the max end pos)
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return intervals;
+        }
+        
+        // sort the intervals based on their start position
+        Arrays.sort(intervals, (i1, i2) -> (i1[0] - i2[0]));
+        
+        List<int[]> merged = new ArrayList<>();
+        int[] pre = intervals[0];
+        merged.add(pre);
+        
+        // compare the interval with the previous one
+        for (int[] interval : intervals) {
+            if (interval[0] <= pre[1]) {
+                //*** change the reference to the second number in pre array
+                pre[1] = Math.max(pre[1], interval[1]);
+            }
+            else {
+                //*** add the origin interval first
+                merged.add(interval);
+                pre = interval;
+            }
+        }
+        
+        int[][] res = new int[merged.size()][2];
+        for (int i = 0; i < merged.size(); i++) {
+            res[i] = merged.get(i);
+        }
+        
+        return res;
+    }
+}
+
 // Time complexity: O(nlogn)
 // Space complexity: O(n)
 class Solution {
