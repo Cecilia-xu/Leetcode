89cec7a0b449c49a401a20fa28430819fbf42042
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-05-25 20:32:49 -0400

    Update Solution.java

--- 1110/Solution.java
+++ 1110/Solution.java
@@ -1,3 +1,6 @@
+// Solution 1: tree + divide and conquer
+// Time complexity: O(n)
+// Space complexity: O(h)
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -33,5 +36,70 @@ class Solution {
             return null;
         }
         return root;
+    }
+}
+// Solution 2: tree + traversal
+// Time complexity: O(n)
+// Space complexity: O(h)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+		List<TreeNode> result = new ArrayList<TreeNode>();
+		if (root == null) {
+	        return result;
+        }
+		if (to_delete.length == 0) {
+			result.add(root);
+			return result;
+        }
+        // put all deleted value into the set
+        HashSet<Integer> deletedSet = new HashSet<>();
+		for (int i : to_delete) {
+			deletedSet.add(i);
+        }
+        // traverse
+        traverse(root, deletedSet, result);
+        if (!deletedSet.contains(root.val)) {
+	            result.add(root);
+            }
+        return result;
+    }
+
+	private void traverse(TreeNode root, HashSet<Integer> deletedSet, List<TreeNode> result) {
+		// base case
+		if  (root == null) {
+	        return;
+        }
+	    if (root.left != null) {
+	        traverse(root.left, deletedSet, result);
+	        if (deletedSet.contains(root.left.val)) {
+	            root.left = null;
+            }
+        }
+
+        if (root.right != null) {
+	        traverse(root.right, deletedSet, result);
+            if (deletedSet.contains(root.right.val)) {
+                root.right = null;
+            }
+        }
+
+        if (deletedSet.contains(root.val)) {
+            if (root.left != null && !deletedSet.contains(root.left.val)) {
+	            result.add(root.left);
+            }
+            if (root.right != null && !deletedSet.contains(root.right.val)) {
+	            result.add(root.right);
+            }
+        }
+
     }
 }
