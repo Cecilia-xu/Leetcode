5cb74d71da4d625e51089ed979b4b392322841b8
Cecilia Xu <42839518+Cecilia-xu@users.noreply.github.com>
2020-05-25 22:04:25 -0400

    Create Solution.java

---
+++ 448/Solution.java
@@ -0,0 +1,18 @@
+// Solution: array - modify the element based on the condition of the question
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = Math.abs(nums[i]);
+            if (nums[num - 1] > 0) {
+                nums[num - 1] = -nums[num - 1];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        return result;
+    }
+}
